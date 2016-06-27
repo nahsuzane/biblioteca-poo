311a4898a673545559c689b639e953ed1ff63f13
andersondcs <andersondcs00@gmail.com>
2016-06-27 13:26:13 -0300

    putitrain

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World2");
 		System.out.println("Mais um teste");
+		System.out.println("Putitrain");
 	}
 }
